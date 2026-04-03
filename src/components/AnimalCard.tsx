db008e2caa8c8f53eea61624e5f7c8011904831c
aandrx <andrewliucodes@gmail.com>
2026-04-03 18:39:00 -0400

    updated components: sidebar/titlebar styling +traininglogcard edit button function

--- src/components/AnimalCard.tsx
+++ src/components/AnimalCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import animalImage from '@images/animalImage.png';
 
 const Animalcard = () => {
-  //
+  //wip
   <div>
     <img src={animalImage.src} alt="Animal" className="w-full h-48 object-cover rounded-t-lg" />
   </div>
