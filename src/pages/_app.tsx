c09fd01593b245f5f2571a1c4d2ca61a55031851
nickrobertson256 <nickrobertson256@gmail.com>
2026-04-05 17:41:50 -0400

    login page UI polishing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from "next/app";
 import { UserProvider } from "../components/UserContext";
+import "../styles/globals.css";
+import { Heebo } from "next/font/google";
+
+const heebo = Heebo({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <main className={heebo.className}>
+        <Component {...pageProps} />
+      </main>
     </UserProvider>
   );
 }
